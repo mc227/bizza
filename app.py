e99ccb0b5b304743e11819ff7b05daa92f6eee6a
mc227 <markcostales.mark@gmail.com>
2024-02-28 02:28:24 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+app.py
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    """app.py"""
+    return 'Oh, Hi Mark!'
+
+if __name__ == '__main__':
+    app.run()
